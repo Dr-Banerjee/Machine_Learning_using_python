8459e49d36daa1d984271e59da84776cb1645221
Dr-Banerjee <raieanmathphd@gmail.com>
2025-02-23 22:03:51 +0100

    Wine-Testing-Deep-Neural-Network.py

--- Wine-Testing-Deep-Neural-Network.py
+++ Wine-Testing-Deep-Neural-Network.py
@@ -21,7 +21,7 @@ input_shape = [8]
 from tensorflow import keras
 from tensorflow.keras import layers
 
-# YOUR CODE HERE
+
 model = keras.Sequential([
     layers.Dense(units=512, activation='relu', input_shape=[8]),
     layers.Dense(units=512, activation='relu'),
