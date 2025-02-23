f539e3f956685029f6316160c9eca1c04998d1d0
Dr-Banerjee <raieanmathphd@gmail.com>
2025-02-23 21:33:28 +0100

    Add files via upload

---
+++ Wine-Testing-Deep-Neural-Network.py
@@ -0,0 +1,50 @@
+import tensorflow as tf
+
+# Setup plotting
+import matplotlib.pyplot as plt
+
+plt.style.use('seaborn-whitegrid')
+# Set Matplotlib defaults
+plt.rc('figure', autolayout=True)
+plt.rc('axes', labelweight='bold', labelsize='large',
+       titleweight='bold', titlesize=18, titlepad=10)
+
+# Setup feedback system
+from learntools.core import binder
+binder.bind(globals())
+from learntools.deep_learning_intro.ex2 import *
+import pandas as pd
+
+concrete = pd.read_csv('../input/dl-course-data/concrete.csv')
+concrete.head()
+input_shape = [8]
+from tensorflow import keras
+from tensorflow.keras import layers
+
+# YOUR CODE HERE
+model = keras.Sequential([
+    layers.Dense(units=512, activation='relu', input_shape=[8]),
+    layers.Dense(units=512, activation='relu'),
+    layers.Dense(units=512, activation='relu'),
+    layers.Dense(units=1),
+
+])
+model = keras.Sequential([
+    layers.Dense(32, input_shape=[8]),
+    layers.Activation('relu'),
+    layers.Dense(32),
+    layers.Activation('relu'),
+    layers.Dense(1),
+])
+
+activation_layer = layers.Activation('swish')
+
+x = tf.linspace(-3.0, 3.0, 100)
+y = activation_layer(x) # once created, a layer is callable just like a function
+
+plt.figure(dpi=100)
+plt.plot(x, y)
+plt.xlim(-3, 3)
+plt.xlabel("Input")
+plt.ylabel("Output")
+plt.show()
